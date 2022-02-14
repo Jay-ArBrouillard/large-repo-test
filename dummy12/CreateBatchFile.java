4d775f9fa24a2b9cda9c52573027e3179f3851fb
Jay-Ar Brouillard <jay-ar.brouillard@flexagon.com>
2022-02-14 12:08:04 -0500

    Add another 1000 files

--- dummy12/CreateBatchFile.java
+++ dummy12/CreateBatchFile.java
@@ -11,7 +11,7 @@ class CreateBatchFile {
 				myObj.createNewFile();
 			}
 			FileWriter myWriter = new FileWriter(myObj);
-			for (int i = 1; i <= 1000; i++) {
+			for (int i = 1; i <= 2000; i++) {
 				myWriter.write("C:\\Users\\developer\\Downloads\\large-repo-test\\TestFile" + i + ".txt\t1024\t1\n");
 			}
       			
